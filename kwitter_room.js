6f2d639b91f1d16aa9b10a430287421fe31af224
Advika123456789 <78788299+Advika123456789@users.noreply.github.com>
2021-09-20 14:46:53 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDzi5oFIgEK-5piG2ruZzsxmVTGg4Hjznk",
+    authDomain: "kelly-tpnv.firebaseapp.com",
+    projectId: "kelly-tpnv",
+    storageBucket: "kelly-tpnv.appspot.com",
+    messagingSenderId: "190722745969",
+    appId: "1:190722745969:web:84c1a52d06cf50e7c91dd0",
+    measurementId: "G-XV3QMVD0BR"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
