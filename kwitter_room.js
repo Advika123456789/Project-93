13335cba30179e76961040194161166d74b96c75
Advika123456789 <78788299+Advika123456789@users.noreply.github.com>
2021-09-20 21:22:55 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,13 +1,43 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDzi5oFIgEK-5piG2ruZzsxmVTGg4Hjznk",
-    authDomain: "kelly-tpnv.firebaseapp.com",
-    projectId: "kelly-tpnv",
-    storageBucket: "kelly-tpnv.appspot.com",
-    messagingSenderId: "190722745969",
-    appId: "1:190722745969:web:84c1a52d06cf50e7c91dd0",
-    measurementId: "G-XV3QMVD0BR"
-  };
-  
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+  apiKey: "AIzaSyDzi5oFIgEK-5piG2ruZzsxmVTGg4Hjznk",
+  authDomain: "kelly-tpnv.firebaseapp.com",
+  databaseURL: "https://kelly-tpnv-default-rtdb.firebaseio.com",
+  projectId: "kelly-tpnv",
+  storageBucket: "kelly-tpnv.appspot.com",
+  messagingSenderId: "190722745969",
+  appId: "1:190722745969:web:683e6f7981cbf8cdc91dd0",
+  measurementId: "G-JHY471VEF7"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+  Room_names = childKey;
+ //Start code
+console.log("Room Name - " + Room_names);
+row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#" + Room_names +"</div><hr>";
+document.getElementById("output").innerHTML += row;
+ //End code
+ });});}
+getData();
+
+
+function addRoom() {
+  room_name = document.getElementById("room_name").value;
+
+  firebase.database().ref("/").child(room_name).update({
+       purpose : "adding room name" 
+  });
+
+  localStorage.setItem("room_name", room_name);
+
+  window.location = "kwitter_page.html";
+}
+
+function redirectToRoomName(name)
+{
+      console.log(name);
+      localStorage.setItem("room_name", name);
+      window.location= "kwitter_page.html";
+}
